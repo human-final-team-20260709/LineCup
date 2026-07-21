2c7b57e772ed389d1d81e1d4ddb514b4a3f5dd2f
YamongE <et3566@naver.com>
2026-07-21 12:31:14 +0900

    Revert "20260721/김형준/품질,통신 백엔드 구현"

--- backend/src/main/java/com/human/linecup/repository/DefectHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.human.linecup.repository;
-
-import com.human.linecup.entity.DefectHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DefectHistoryRepository extends JpaRepository<DefectHistory, Long> {
-}
