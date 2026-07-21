19c87b10b200dde94a4286b45205a525f3e41a99
gold4877 <gold4877@naver.com>
2026-07-21 12:17:19 +0900

    20260721 - 오금빈 / User/Product/Equipment 관련 파일 삭제

--- backend/src/main/java/com/human/linecup/repository/EquipmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.human.linecup.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.human.linecup.entity.Equipment;
-
-public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
-}
--- backend/src/main/java/com/human/linecup/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.human.linecup.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.human.linecup.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
